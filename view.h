e04a34c84500bde2a157ea0fe0d9c845e925e192
CognitorTenebras <demon.mozgunov@gmail.com>
2015-01-15 15:36:49 +0300

    Second commit

---
+++ view.h
@@ -0,0 +1,18 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <QWidget>
+
+class view : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit view(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // VIEW_H
